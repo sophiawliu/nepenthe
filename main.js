b069727ddc3cce3baced014063f1b6a3d341c914
Sophia Liu <sophiawliu@berkeley.edu>
2023-08-01 21:02:21 -0700

    Update main.js

--- main.js
+++ main.js
@@ -86,4 +86,25 @@ function createContact() {
     content.appendChild(contact);
 }
 
-export {createHome, createMenu, createAbout, createContact};
+createHome();
+
+const header = document.querySelector('#header');
+const navbar = document.querySelector('#navbar');
+const nepentheLogo = document.querySelector('#logo-header');
+const homeButton = document.querySelector('.tab.home');
+const menuButton = document.querySelector('.tab.menu');
+const aboutButton = document.querySelector('.tab.about');
+const contactButton = document.querySelector('.tab.contact');
+
+nepentheLogo.addEventListener('click', createHome);
+homeButton.addEventListener('click', createHome);
+menuButton.addEventListener('click', createMenu);
+aboutButton.addEventListener('click', createAbout);
+contactButton.addEventListener('click', createContact);
+
+header.appendChild(nepentheLogo);
+navbar.appendChild(homeButton);
+navbar.appendChild(menuButton);
+navbar.appendChild(aboutButton);
+navbar.appendChild(contactButton);
+
